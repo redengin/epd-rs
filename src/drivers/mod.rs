04a018f7050d4de60de18acd4e8e42db78c0a0e6
redengin <sholstei@yahoo.com>
2026-09-05 00:02:17 -0400

    in work

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,9 +1,11 @@
-
+#[allow(non_snake_case)]
 mod _E0213A367;
 pub use _E0213A367::E0213A367 as E0213A367;
 
+// #[allow(non_snake_case)]
 // mod _DEPG0213BNS800;
 // pub use _DEPG0213BNS800::DEPG0213BNS800 as DEPG0213BNS80;
 
+// #[allow(non_snake_case)]
 // mod _LCMEN2R13EFC;
 // pub use _LCMEN2R13EFC::LCMEN2R13EFC as LCMEN2R13EFC;
